9a66094734adfe5365d6087c7035073daf062f4e
Ratnakardagar <rinkudagar713@gmail.com>
2024-06-03 17:00:35 +0530

    Add files via upload

---
+++ src/components/banner/LeftBanner.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiJavascript, SiHtml5, SiCss3 } from "react-icons/si";
+
+const LeftBanner = () => {
+    const [text] = useTypewriter({
+      words: ["Professional Coder.", "Web Developer.", "UI Designer."],
+      loop: true,
+      typeSpeed: 20,
+      deleteSpeed: 10,
+      delaySpeed: 2000,
+    });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">Ratnakar Dagar</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+        I’m passionate about web development, coding, and problem-solving. I love diving into new technologies and exploring innovative solutions. As a quick learner, I thrive in dynamic environments and enjoy collaborating with fellow tech enthusiasts. 💡
+        </p>
+      </div>
+      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaTwitter />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiHtml5 />
+            </span>
+            <span className="bannerIcon">
+              <SiCss3 />
+            </span>
+            <span className="bannerIcon">
+              <SiJavascript />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftBanner
---
+++ src/components/resume/Education.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { motion } from "framer-motion";
+import ResumeCard from "./ResumeCard";
+
+const Education = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+    >
+      {/* part one */}
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]"></p>
+          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="B-Tech in Computer Science"
+            subTitle="DCRUST (2022 - 2026)"
+            result="7 SGPA"
+            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+          />
+          <ResumeCard
+            title=" Senior Secondary School Education"
+            subTitle="Vidya Niketan Sr. Secondary School (2021 - 2022)"
+            result="70%"
+            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+          />
+          <ResumeCard
+            title="Secondary School Education"
+            subTitle="Vidya Niketan Sr. Secondary School (2019 - 2020)"
+            result="82%"
+            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+          />
+        </div>
+      </div>
+      {/* part Two */}
+
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]"></p>
+          <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          
+          <ResumeCard
+            title="Front-end Developer"
+            subTitle="Bharat Intern"
+            result="Done"
+            des="it is virtual internship "
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Education;
